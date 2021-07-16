fdbc706f2dea0ed1c9d07b6aa6981410f54ed866
Yurikel <zog@yurikel.ru>
2021-07-16 14:08:29 +0300

    adding seeder

--- back/app.js
+++ back/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const connect = require("./db/connect");
-const coonect = require("./db/connect")
+const seedDatabase = require("./db/seeder")
+const path = require('path')
 
 
 const app = express();
 connect();
+seedDatabase();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
--- back/db/models/good.model.js
+++ back/db/models/good.model.js
@@ -1,19 +1,18 @@
 const mongoose = require("mongoose");
 
 const goodSchema = mongoose.Schema({
-  title: { 
-    type: String, 
-    required: true
+  title: {
+    type: String,
+    required: true,
   },
-  price: { 
-    type: Number, 
-    required: true
+  price: {
+    type: Number,
+    required: true,
   },
-  stock: { 
-    type: Number, 
-    required: true
+  stock: {
+    type: Number,
+    default: 0,
   },
-  
 });
 
 module.exports = mongoose.model("Good", goodSchema);
