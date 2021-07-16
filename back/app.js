ff39be00d5971ecda915c18c4b886cf4a0f7456b
alexten1st <aaten1994@mail.ru>
2021-07-16 12:09:40 +0300

    routes

--- back/app.js
+++ back/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const connect = require("./db/connect");
-const coonect = require("./db/connect")
+const coonect = require("./db/connect");
+const mainRote = require("./routes/main");
+const adminRote = require("./routes/admin");
+const agentRoute = require("./routes/agent")
+
 
 
 const app = express();
@@ -8,6 +12,9 @@ connect();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/", mainRote);
+app.use("/admin",adminRote);
+app.use("/agent", agentRoute);
 
 
 module.exports = app
--- back/server.js
+++ back/server.js
@@ -6,6 +6,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const server = http.createServer(app);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`));
