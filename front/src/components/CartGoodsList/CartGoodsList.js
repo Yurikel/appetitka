6a18cf047d4a4b2ecd1cb43da87b6262850a8f0b
Yurikel <zog@yurikel.ru>
2021-07-20 16:46:18 +0300

    rightpanel rerussection

--- front/src/components/CartGoodsList/CartGoodsList.js
+++ front/src/components/CartGoodsList/CartGoodsList.js
@@ -1,35 +1,37 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Good from "../Good/Good";
 import { clearCartAC } from "../../utils/redux/actionCreators";
-import { getCookie } from 'react-use-cookie';
+import { getCookie } from "react-use-cookie";
 
 export default function CartGoodsList() {
+  const dispatch = useDispatch();
+  const rightPanelDiv = useRef();
+
   let user = useSelector((state) => state.agentReducer.currentUser);
   let cart = useSelector((state) => state.agentReducer.cart);
   let goods = useSelector((state) => state.goodsReducer.goods);
-  const dispatch = useDispatch();
-  function totalCost(){
+
+  function totalCost() {
     let result = 0;
-     cart.forEach(el =>{
-      let price = 0; 
-      for(let i=0; i<goods.length; i++){
-        if (goods[i].title == el.title){
-          price = +goods[i].price
+    cart.forEach((el) => {
+      let price = 0;
+      for (let i = 0; i < goods.length; i++) {
+        if (goods[i].title == el.title) {
+          price = +goods[i].price;
         }
       }
-      result += price * el.value
-    })
+      result += price * el.value;
+    });
     return result;
   }
-  
-   let total = totalCost();
+
   function makeApplication() {
     // console.log(cart);
     fetch(`http://localhost:4000/agent/cart/${user}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({goods:cart}),
+      body: JSON.stringify({ goods: cart }),
     })
       .then((res) => res.json())
       .then((data) => alert(data.message));
@@ -40,8 +42,18 @@ export default function CartGoodsList() {
     dispatch(clearCartAC(""));
   };
 
-  // const calculateTotal = () => {
-  // };
+  const [divMargin, setDivMargin] = useState(0);
+
+  useEffect(() => {
+    const onScroll = (e) => {
+      setDivMargin(
+        e.target.documentElement.scrollTop - rightPanelDiv.current.offsetTop
+      );
+    };
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [setDivMargin]);
 
   return (
     <div className="inline">
@@ -53,11 +65,12 @@ export default function CartGoodsList() {
           />
         ))}
       </div>
-      <div className="confirmblock">
+      <div className="confirmblock" ref={rightPanelDiv}>
+        <div className={divMargin > 0 && "fixed"}  style={{top: 10}}>
         {cart.length ? (
           <>
             <h4>Ориентировочная стоимость заказа:</h4>
-            <h2>100 ₽</h2>
+            <div className="sum">₽ {totalCost()}</div>
             <textarea placeholder="Комментарий к заказу..."></textarea>
             <button onClick={makeApplication} className="button primary">
               Сформировать заявку
@@ -69,6 +82,7 @@ export default function CartGoodsList() {
         ) : (
           <div>Ваша корзина пуста</div>
         )}
+        </div>
       </div>
     </div>
   );
